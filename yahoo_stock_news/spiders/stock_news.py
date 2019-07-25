419cfc14626d617500d52f9d56f0b94d4bd133da
jimmyhsu1010 <jimmyhsu1010@gmail.com>
2019-07-25 19:35:37 +0800

    comment added regarding how to define different pipelines for various spiders

--- yahoo_stock_news/spiders/stock_news.py
+++ yahoo_stock_news/spiders/stock_news.py
@@ -16,7 +16,8 @@ class StockNewsSpider(scrapy.Spider):
     start_urls = []
     custom_settings = {
         'ITEM_PIPELINES': {'yahoo_stock_news.pipelines.YahooStockNewsPipeline':300}
-    }
+    } # 多個爬蟲對應不同的item和pipeline時可以在每個爬蟲使用custom_settings定義要使用那個pipeline
+
     # with open("/Users/kai/Documents/scrapingEnv/tw_stock/tw_stock/spiders/臺灣上市公司代碼表2019-07-19.csv", newline="") as f:
     #     data = csv.reader(f, delimiter="\t")
     data = df[0]
@@ -30,26 +31,17 @@ class StockNewsSpider(scrapy.Spider):
         bs = BeautifulSoup(response.text)
         item['company'] = bs.find("div", {"style":"display: inline-block"}).text
         item['code'] = bs.find("div", {"style":"margin-left: 8px; color: #979ba7; display: inline-block"}).text.strip()
-        # print(company, code)
         contents = response.xpath("/html[1]/body[1]/center[1]/table[1]/tbody[1]/tr[1]/td[1]/table[2]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr[2]/td[1]/table[1]/tbody[1]/tr/td[2]")
-        # print(contents)
         ref = "https://tw.stock.yahoo.com"
         for content in contents:
             title = content.xpath("./a[1]//@href").extract()
             if title[0] != "/news/":
                 end = title[0]
                 url = ref + end
-                # print(item, url)
                 yield scrapy.Request(url=url, meta={"item":item}, callback=self.parse_content, dont_filter=True)
 
 
     def parse_content(self, response):
-        # with open("stock_news.json", "r") as f:
-        #     for line in f.readlines():
-        #         data = json.loads(line)
-        #         if response.url in data['url']:
-        #             print("有重複了，不抓取，你這個白癡！")
-        #             pass
         item = response.meta['item']
         item['title'] = response.xpath("//h1[1]/text()").extract()[0]
         text = response.xpath("//td[1]/p//text()").extract()
